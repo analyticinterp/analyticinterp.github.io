a211e0d0e37b9e9eb248679ac602d8fc2b172922
Alexandru Meterez <alexandrumeterez@gmail.com>
2025-06-21 17:17:48 -0400

    no waline

--- src/config.ts
+++ src/config.ts
@@ -93,7 +93,7 @@ export const themeConfig: ThemeConfig = {
     // https://waline.js.org/en/
     waline: {
       // server url
-      serverURL: 'https://analyticinterp.github.io',
+      serverURL: '',
       // emoji url
       emoji: [
         'https://unpkg.com/@waline/emojis@1.2.0/tw-emoji',
